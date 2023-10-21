c085ab7413bc0fb04043c4ee40442a6b9709acf3
LucasDantas77 <lucasdantasrodrigues847@gmail.com>
2023-10-20 23:35:33 -0300

    landing page finalizada

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,17 +1,23 @@
 import Logo from "../../assets/Logo.jpeg";
+import { Container, DivContainer, DivNames } from "./style";
 
 export const Fotter = () => {
   return (
-    <footer>
-      <div>
-        <h3>Fundado por</h3>
-        <p>Phelipe</p>
-        <p>Denis Guedes</p>
-        <p>Gean Santos</p>
-        <p>Lucas Dantas</p>
-      </div>
-      <img src={Logo} alt="Logo" />
+    <Container>
+      <DivContainer>
+        <DivNames>
+          {" "}
+          <h3>Fundado por:</h3>
+          <p>Phelipe Lani</p>
+          <p>Denis Guedes</p>
+          <p>Gean Santos</p>
+          <p>Lucas Dantas</p>
+        </DivNames>
+
+        <img src={Logo} alt="Logo" />
+      </DivContainer>
+
       <span>@2023</span>
-    </footer>
+    </Container>
   );
 };
--- src/components/footer/style.js
+++ src/components/footer/style.js
@@ -1,3 +1,46 @@
 import styled from "styled-components";
 
-export const Container = styled.footer``;
+export const Container = styled.footer`
+  width: 100%;
+  height: 25%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+
+  span {
+    font-size: 12px;
+    color: gray;
+    margin-bottom: 10px;
+  }
+`;
+
+export const DivContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 60px;
+  align-items: center;
+  height: 100%;
+  img {
+    max-width: 100px;
+    width: 30%;
+  }
+
+  @media (min-width: 768px) {
+    gap: 900px;
+  }
+`;
+
+export const DivNames = styled.div`
+  display: flex;
+  flex-direction: column;
+  h3 {
+    font-size: 15px;
+    color: gray;
+  }
+
+  p {
+    font-size: 12px;
+    color: gray;
+  }
+`;
